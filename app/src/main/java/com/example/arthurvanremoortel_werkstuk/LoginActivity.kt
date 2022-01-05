2ee998312a4d9e3539709522e868d0b84a5df14a
ArthurVanRemoortel <arthurvanremoortel@gmail.com>
2022-01-05 19:58:16 +0100

    Feat: Implemented basic firebase registration and login

---
+++ app/src/main/java/com/example/arthurvanremoortel_werkstuk/LoginActivity.kt
@@ -0,0 +1,75 @@
+package com.example.arthurvanremoortel_werkstuk
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Toast
+import com.example.arthurvanremoortel_werkstuk.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityLoginBinding
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val myRoot = binding.root
+        setContentView(myRoot)
+        auth = Firebase.auth
+
+        binding.loginButton.setOnClickListener {
+            this.handleLogin()
+        }
+
+        binding.registerText.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java)) // TODO: Maybe dismiss current activity instead of adding new on.
+        }
+    }
+
+
+    fun handleLogin(){
+        val email = binding.emailInput.text.toString().trim()
+        val password = binding.passwordInput.text.toString().trim()
+
+        if (TextUtils.isEmpty(email)){
+            binding.emailInput.setError("Email is required.")
+            return
+        }
+        if (TextUtils.isEmpty(password)){
+            binding.passwordInput.setError("Password is required.")
+            return
+        }
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("LOGIN", "signInWithEmail:success")
+                    val user = auth.currentUser
+                    startActivity(Intent(this, MainActivity::class.java))
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("LOGIN", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    // TODO: Show reason for fail.
+                }
+            }
+    }
+
+}
---
+++ app/src/main/java/com/example/arthurvanremoortel_werkstuk/RegisterActivity.kt
@@ -0,0 +1,90 @@
+package com.example.arthurvanremoortel_werkstuk
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Toast
+import com.example.arthurvanremoortel_werkstuk.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityRegisterBinding
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        val myRoot = binding.root
+        setContentView(myRoot)
+
+        auth = Firebase.auth
+
+        binding.registerButton.setOnClickListener {
+            this.handleRegister()
+        }
+
+        binding.registerText.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+
+
+    fun handleRegister() {
+        val email = binding.emailInput.text.toString().trim()
+        val password = binding.passwordInput.text.toString().trim()
+        val confirmPassword = binding.passwordConfirmInput.text.toString().trim()
+
+        if (TextUtils.isEmpty(email)){
+            binding.emailInput.setError("Email is required.")
+            return
+        }
+        if (TextUtils.isEmpty(password)){
+            binding.passwordInput.setError("Password is required.")
+            return
+        }
+        if (password.length < 6){
+            binding.passwordConfirmInput.setError("Password must be longer than 6 characters")
+            return
+        }
+        if (TextUtils.isEmpty(confirmPassword)){
+            binding.passwordConfirmInput.setError("Confirm password is required.")
+            return
+        }
+        if (password != confirmPassword){
+            binding.passwordConfirmInput.setError("Passwords are not the same")
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("LOGIN", "createUserWithEmail:success")
+                    finish()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("LOGIN", "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    // TODO: Show reason for fail.
+                }
+            }
+    }
+}
