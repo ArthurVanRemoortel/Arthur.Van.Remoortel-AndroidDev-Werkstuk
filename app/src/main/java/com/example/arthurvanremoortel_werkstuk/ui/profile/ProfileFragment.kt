12a4c019f507fd718d34fd3bca6b56ecb5b46e80
ArthurVanRemoortel <arthurvanremoortel@gmail.com>
2022-01-09 18:18:17 +0100

    Feat: Added nl_BE language support

--- app/src/main/java/com/example/arthurvanremoortel_werkstuk/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/arthurvanremoortel_werkstuk/ui/profile/ProfileFragment.kt
@@ -4,11 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.arthurvanremoortel_werkstuk.databinding.FragmentProfileBinding
+
 
 class ProfileFragment : Fragment() {
 
@@ -28,12 +27,9 @@ class ProfileFragment : Fragment() {
             ViewModelProvider(this).get(ProfileViewModel::class.java)
 
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+        val root: View = binding.root
         return root
     }
 
